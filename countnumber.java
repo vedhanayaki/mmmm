b15951a6d8d838723e47b47198e3f7e7f676e213
vedhanayaki <vedhanayaki1996@gmail.com>
2017-06-29 12:41:26 +0530

    Add files via upload

---
+++ countnumber.java
@@ -0,0 +1,12 @@
+package guviset3;
+public class countnumber {
+
+	public static void main(String[]args){
+		int a=1234567890;
+		int d=(int)Math.log10(a+1);
+		System.out.println(d);
+		}
+		
+	}
+
+
